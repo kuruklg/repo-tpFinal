a60ca9a0f5b79e76a6576dc6bb9ab2b9dcdb9630
Karen <kuru_gutierrez@hotmail.com>
2023-07-02 04:01:58 -0300

    Creacion del proyecto final

---
+++ tpFinal_Grupo16/src/main/java/ar/edu/unju/fi/TpFinalGrupo16Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalGrupo16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalGrupo16Application.class, args);
+	}
+
+}
