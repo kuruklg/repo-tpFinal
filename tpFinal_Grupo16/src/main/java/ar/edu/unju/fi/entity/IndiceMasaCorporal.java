e81a9e675054c096359b7cee09f05387bc48db87
aleyufra <alejandromaryufra@gmail.com>
2023-07-09 00:35:44 -0300

    se crea la clase indiceMasaCorporal

--- tpFinal_Grupo16/src/main/java/ar/edu/unju/fi/entity/IndiceMasaCorporal.java
+++ tpFinal_Grupo16/src/main/java/ar/edu/unju/fi/entity/IndiceMasaCorporal.java
@@ -23,13 +23,68 @@ public class IndiceMasaCorporal {
 	@Column(name = "ind_id")
 	private Long id;
 	
-	@Column(name = "sucu_fechaInauguracion", nullable = false)
+	@Column(name = "ind_fecha", nullable = false)
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private LocalDate fechaImc;
 	
+	@Column(name = "ind_usu")
+	private Usuario usuraio;
 	
+	@Column(name = "ind_estado")
+	private boolean estado;
 	
+	/** Constructor por defecto
+	 * 
+	 */
+	public IndiceMasaCorporal() {}
+
+	/** Contructor sobrecargado
+	 * 
+	 * @param id
+	 * @param fechaImc
+	 * @param usuraio
+	 * @param estado
+	 */
+	public IndiceMasaCorporal(Long id, LocalDate fechaImc, Usuario usuraio, boolean estado) {
+		this.id = id;
+		this.fechaImc = fechaImc;
+		this.usuraio = usuraio;
+		this.estado = estado;
+	}
 	
+		
+	/* getter and setters */
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getFechaImc() {
+		return fechaImc;
+	}
+
+	public void setFechaImc(LocalDate fechaImc) {
+		this.fechaImc = fechaImc;
+	}
+
+	public Usuario getUsuraio() {
+		return usuraio;
+	}
+
+	public void setUsuraio(Usuario usuraio) {
+		this.usuraio = usuraio;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
 	
 }
 
